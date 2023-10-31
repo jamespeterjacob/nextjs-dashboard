8bcfc1c03bf07262c6163b6a74e6714ae4088841
jamespeterjacob <jamespeterjacob@gmail.com>
2023-10-31 05:45:55 -0700

    modified:   app/dashboard/(overview)/loading.tsx modified:   app/dashboard/(overview)/page.tsx modified:   app/ui/dashboard/cards.tsx modified:   app/ui/dashboard/latest-invoices.tsx modified:   app/ui/dashboard/revenue-chart.tsx renamed:    app/ui/skeletons.tsx -> app/ui/dashboard/skeletons.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,22 +1,30 @@
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
+// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import {
-    fetchRevenue,
-    fetchLatestInvoices,
-    fetchCardData,
-  } from '@/app/lib/data';
+  RevenueChartSkeleton,
+  InvoiceSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/dashboard/skeletons';
+import { lusitana } from '@/app/ui/fonts';
+// import { 
+//     fetchRevenue,
+//     fetchLatestInvoices,
+//     fetchCardData,
+//   } from '@/app/lib/data';
+  import { Suspense } from 'react';
+  //import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/dashboard/skeletons';
+
  
 export default async function Page() {
-    const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+    //const revenue = await fetchRevenue();
+  //const latestInvoices = await fetchLatestInvoices();
+  // const {
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  // } = await fetchCardData();
 
   return (
     <main>
@@ -34,8 +42,14 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <RevenueChart revenue={revenue} /> */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
