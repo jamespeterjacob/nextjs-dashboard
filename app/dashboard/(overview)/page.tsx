7b787a709fed9a995ddfa5757957ab82ef0daff1
jamespeterjacob <jamespeterjacob@gmail.com>
2023-10-31 07:54:27 -0700

    modified:   app/dashboard/(overview)/page.tsx modified:   app/ui/dashboard/cards.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,30 +1,26 @@
 import { Card } from '@/app/ui/dashboard/cards';
-// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import {
-  RevenueChartSkeleton,
-  InvoiceSkeleton,
-  CardsSkeleton,
-} from '@/app/ui/dashboard/skeletons';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+
 import { lusitana } from '@/app/ui/fonts';
-// import { 
-//     fetchRevenue,
-//     fetchLatestInvoices,
-//     fetchCardData,
-//   } from '@/app/lib/data';
+import { 
+    //fetchRevenue,
+    //fetchLatestInvoices,
+    fetchCardData,
+  } from '@/app/lib/data';
   import { Suspense } from 'react';
-  //import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/dashboard/skeletons';
+  import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/dashboard/skeletons';
 
  
 export default async function Page() {
     //const revenue = await fetchRevenue();
   //const latestInvoices = await fetchLatestInvoices();
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
 
   return (
     <main>
