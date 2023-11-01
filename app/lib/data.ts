3eb0230bff5a12f56c6873637c394010887da005
jamespeterjacob <jamespeterjacob@gmail.com>
2023-11-01 16:08:56 -0700

    new file:   app/dashboard/invoices/[id]/edit/not-found.tsx modified:   app/dashboard/invoices/[id]/edit/page.tsx new file:   app/dashboard/invoices/error.tsx modified:   app/lib/actions.ts modified:   app/lib/data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -163,17 +163,18 @@ export async function fetchInvoiceById(id: string) {
         invoices.status
       FROM invoices
       WHERE invoices.id = ${id};
-    `;
+    `;  
 
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); 
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoice.');
   }
 }
 
