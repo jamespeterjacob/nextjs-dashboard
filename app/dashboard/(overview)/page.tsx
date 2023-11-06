4b01b393d508b0eaa5f55ec6c46e8488fab2034e
jamespeterjacob <jamespeterjacob@gmail.com>
2023-11-05 23:50:13 -0800

    modified:   app/dashboard/(overview)/page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,12 @@
-import { Card } from '@/app/ui/dashboard/cards';
+//import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
- import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
- import CardWrapper from '@/app/ui/dashboard/cards';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import CardWrapper from '@/app/ui/dashboard/cards';
 
 import { lusitana } from '@/app/ui/fonts';
 import { 
     //fetchRevenue,
-    fetchLatestInvoices,
+    //fetchLatestInvoices,
     fetchCardData,
   } from '@/app/lib/data';
   import { Suspense } from 'react';
@@ -15,7 +15,7 @@ import {
  
 export default async function Page() {
     //const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  //const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -46,7 +46,7 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
