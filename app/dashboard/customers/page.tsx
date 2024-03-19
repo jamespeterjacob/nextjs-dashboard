79787987d1895e1f268a03473db117e53c76c405
jamespeterjacob <jamespeterjacob@gmail.com>
2024-03-19 15:29:13 -0700

    customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,23 +3,40 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/customers/table';
 import { CreateCustomer } from '@/app/ui/customers/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchCustomersPages } from '@/app/lib/data';
 import Image from 'next/image';
 import { customers } from '@/app/lib/placeholder-data';
+
  
-export default async function Page() {
-  
-  const custs = await fetchCustomers();
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-        {custs.map((customers) =>
-      <div key={customers.id}>{customers.name}</div> 
-      )} 
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+        <CreateCustomer />
+        
+      </div>
+      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+      <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
