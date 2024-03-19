965b784cedbdade607a1f646e5cbf317e48c5aef
jamespeterjacob <jamespeterjacob@gmail.com>
2024-03-19 09:03:39 -0700

    ree

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -10,37 +10,16 @@ import Image from 'next/image';
 import { customers } from '@/app/lib/placeholder-data';
  
 export default async function Page() {
-  /* searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) { */
-  //const query = searchParams?.query || '';
-  //const currentPage = Number(searchParams?.page) || 1;
+  
   const custs = await fetchCustomers();
 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search customers..." />
-        <CreateCustomer />
-        
-      </div>
-      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table  />
-      </Suspense> */}
-      {custs.map((customers) =>
+        {custs.map((customers) =>
       <div key={customers.id}>{customers.name}</div> 
       )}
-
-
-      <div className="mt-5 flex w-full justify-center">
-     {/*  <Pagination totalPages={totalPages} /> */}
       </div>
     </div>
   );
