e37159b9fa710d09992723535f2fb85d01a1364a
jamespeterjacob <jamespeterjacob@gmail.com>
2024-03-19 15:35:13 -0700

    te

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -29,15 +29,15 @@ export default async function Page({
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search customers..." />
-        <CreateCustomer />
+       {/*  <CreateCustomer /> */}
         
       </div>
-      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+     {/*  <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
       <Pagination totalPages={totalPages} />
-      </div>
+      </div> */}
     </div>
   );
 }
