9100e18c194c2c852df6e15117b30c88de4ca9cd
jamespeterjacob <jamespeterjacob@gmail.com>
2024-03-20 05:58:32 -0700

    revertInvpage

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,12 @@
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+//import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchCustomers, fetchInvoicesPages } from '@/app/lib/data';
+//import { fetchCustomers, fetchInvoicesPages } from '@/app/lib/data';
 import Image from 'next/image';
 import { customers } from '@/app/lib/placeholder-data';
 
@@ -20,7 +21,8 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  const customers = await fetchCustomers();
+  const totalPages = await fetchInvoicesPages(query);
+  //const customers = await fetchCustomers();
   
 
   return (
@@ -33,8 +35,11 @@ export default async function Page({
         <CreateInvoice />
         
       </div>
-      
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
+      <Pagination totalPages={totalPages} />
       {/* <Pagination totalPages={totalPages} /> */}
       </div>
     </div>
