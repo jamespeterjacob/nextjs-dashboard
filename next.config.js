8dc762ea960c96863ed1e1564f97bd21b2d28260
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-08 04:06:37 -0700

    images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //output: 'export',
+    images: {
+        domains: ['nextjs-dashboard-orcin-eta.vercel.app/dashboard/customers/**'],
+    },
 };
 
-module.exports = nextConfig
+export default nextConfig
