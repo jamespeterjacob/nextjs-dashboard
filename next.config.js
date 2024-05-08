967035e04f46bfef67372b39ffc559a9aa064cdb
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-08 04:17:50 -0700

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //output: 'export',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'nextjs-dashboard-orcin-eta.vercel.app',
-                port: '',
-                pathname: '/dashboard/customers/**',
-            },
-        ],
-    },
 };
 
-export default nextConfig
+module.exports = nextConfig
