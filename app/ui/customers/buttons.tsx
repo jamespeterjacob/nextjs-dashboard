c76418333350af134950919be0d450be9c00512a
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-08 19:16:44 -0700

    modified:   app/ui/customers/buttons.tsx

--- app/ui/customers/buttons.tsx
+++ app/ui/customers/buttons.tsx
@@ -1,6 +1,5 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { deleteCustomer } from '@/app/lib/actions';
 
 export function CreateCustomer() {
   return (
@@ -17,7 +16,7 @@ export function CreateCustomer() {
 export function UpdateCustomer({ id }: { id: string }) {
   return (
     <Link
-    href={`/dashboard/customers/${id}/edit`}
+      href="/dashboard/customers"
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
@@ -29,14 +28,11 @@ export function DeleteCustomer({ id }: { id: string }) {
   const deleteCustomerWithId = deleteCustomer.bind(null, id);
   //test
   return (
-    
-    <form action={deleteCustomerWithId}>
+    <>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
-        <TrashIcon className="w-4" />
+        <TrashIcon className="w-5" />
       </button>
-    </form>
-    
-  
+    </>
   );
 }
