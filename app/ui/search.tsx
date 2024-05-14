b294d30fd472ba5844eca8d812f7b92d918ee165
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-14 02:29:35 -0700

    modified:   app/ui/invoices/pagination.tsx 	modified:   app/ui/search.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -11,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
