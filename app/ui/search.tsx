25c3e67e2c9c18cf4a642a25d6ee3405a5ac2cd9
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-22 01:52:02 -0700

    modified:   app/ui/search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch (term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
+    //const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
@@ -20,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
+  }
+  //, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -38,4 +39,5 @@ export default function Search({ placeholder }: { placeholder: string }) {
     <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
   </div>
   );
-}}
+}
+//}
