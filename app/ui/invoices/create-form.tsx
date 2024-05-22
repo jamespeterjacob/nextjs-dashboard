d50f8daba79e38b028e09f53cb79221cde403206
jamespeterjacob <jamespeterjacob@gmail.com>
2024-05-22 01:49:03 -0700

    modified:   app/ui/invoices/create-form.tsx 	modified:   app/ui/invoices/edit-form.tsx

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -2,12 +2,7 @@
 
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon, } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
